d9407b8dd5961f2a6aef0d9cf1be8876b6541f92
LeMinux <MatthewLoz@protonmail.com>
2025-06-23 22:02:04 -0400

    Adjusted printed format and fixed printing for cd and echo

--- Files/Linux/DirectoryPermissions/perms.sh
+++ Files/Linux/DirectoryPermissions/perms.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+#This is a script to show how directory permissions determine what can be done to the directory
+#there is also a file used as an example to show how a file's permissions appear to rebel against
+#the directory's permissions, but remember the directory is a list of inodes.
+
 #colors
 cyan="\033[0;36m"
 nocolor="\033[0m"
@@ -7,40 +13,68 @@ red="\033[0;31m"
 dir="TestDir"
 file="file.txt"
 rename="renamed.txt"
-known="cheese.txt"
+known="known.txt"
+executable="some_script.sh"
 
 printOffStatus() {
-    $@
+    #short hand if/else
+    #&& excutes if the exit status is 0 (success)
+    #if not it will go to the || and say it failed
+    "$@" && echo -e "\t${green}Success: $@${nocolor}" || echo -e "\t${red}Failed: $@${nocolor}"
+}
+
+#These functions below have special behavior that the normal function can't account for
+#back ground process enters later and prints so needs a wait
+printCDStatus() {
+    cd "$dir" &
+    wait $!
     if [[ $? == 0 ]]; then
-        echo -e "${green}Success: $@${nocolor}"
+        echo -e "\t${green}Success: cd${nocolor}"
     else
-        echo -e "${red}Failed: $@${nocolor}"
+        echo -e "\t${red}Failed: cd${nocolor}"
     fi
 }
 
-printOffStatusBackground() {
-    $@
-    if [[ $! == 0 ]]; then
-        echo -e "${green}Success: $@${nocolor}\n"
+#redirection is applied to the function call not the command if used on one line
+printEchoStatus() {
+    echo "I am a known file by direct path" > "$dir/$known"
+    if [[ $? == 0 ]]; then
+        echo -e "\t${green}Success: echo${nocolor}"
     else
-        echo -e "${red}Failed: $@${nocolor}\n"
+        echo -e "\t${red}Failed: echo${nocolor}"
     fi
 }
 
 runTests() {
-     printOffStatus touch "$dir/$file"
-     printOffStatus mv "$dir/$file" "$dir/$rename" && rm "$dir/$rename"
-     printOffStatus ls "$dir"
-     printOffStatus ls -al "$dir"
-     printOffStatus cat "$dir/$known"
-     printOffStatus echo "I am cheese" > "$dir/$known"
-     printOffStatusBackground cd "$dir" &
-     echo ""
+    echo  "Testing directory write permission"
+    printOffStatus touch "$dir/$file" && mv "$dir/$file" "$dir/$rename" && rm "$dir/$rename"
+    printOffStatus touch -c "$dir/$known"
+    echo  "Testing directory read permission"
+    printOffStatus ls "$dir"
+    printOffStatus ls -al "$dir"
+    echo  "Testing directory execute permission"
+    printCDStatus
+
+    echo  "Testing file read permission"
+    printOffStatus cat "$dir/$known"
+    echo  "Testing file write permission"
+    printEchoStatus
+    echo  "Testing file execute permissions"
+    printOffStatus ./"$dir/$executable"
+    echo ""
 }
 
 main() {
     if [[ ! -d "$dir" ]]; then
         mkdir "$dir"
+    fi
+
+    if [[ ! -f "$dir/$known" ]]; then
+        echo "Have read the file" > "$dir/$known" && chmod 600 "$dir/$known"
+    fi
+
+    if [[ ! -f "$dir/$executable" ]]; then
+        echo 'echo "executed the script"' > "$dir/$executable" && chmod 500 "$dir/$executable"
     fi
 
     #rwx
@@ -87,4 +121,5 @@ main() {
     chmod 700 "$dir"
 }
 
-main
+#tab error output to align with my tabbed input
+main 2> >(sed 's/^/\t/' >&2)
