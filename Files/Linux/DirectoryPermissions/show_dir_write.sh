c371aff06dc3aada72ed8a25c54af0c247413b23
LeMinux <MatthewLoz@protonmail.com>
2025-06-24 21:13:17 -0400

    Addded script to show directory removal based on write

---
+++ Files/Linux/DirectoryPermissions/show_dir_write.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#colors
+cyan="\033[0;36m"
+nocolor="\033[0m"
+
+dir="ShowWritePerms"
+file="some_file.txt"
+root_file="some_root_file.txt"
+
+main() {
+    mkdir -p "$dir"
+    touch "$dir/$file"
+    touch "$dir/$root_file"
+    echo "Sudo permissions required to change $dir/$root_file to root:root."
+    sudo chown root:root "$dir/$root_file"
+
+    echo -e "${cyan}Contents before${nocolor}"
+    ls -al "$dir"
+
+    echo -e "\nremoving file owned by $(whoami)"
+    rm "$dir/$file"
+
+    echo -e "removing file owned by root"
+    rm "$dir/$root_file"
+
+    echo -e "\n${cyan}Contents after${nocolor}"
+    ls -al "$dir"
+}
+
+main
